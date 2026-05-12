4d0080779ebc4f1246d16de1d9868603153128b3
Syed-Hamza13 <syed13hamza@gmail.com>
2026-05-12 19:52:29 +0530

    feat(complaints): add complaint classification feature and integrate axios for API calls

--- src/controllers/citizenController.js
+++ src/controllers/citizenController.js
@@ -1,19 +1,33 @@
+let complaintList = [];
+import { classifyComplaint } from "../../python-services/api/classifier_model.js";
 export async function submitComplaint(req, res) {
-    
   const complaint = req.body;
   const complaintHeaders = req.headers;
-  const citizenSession = req.session // Assuming user ID is available in req.user
-  const citizenId = req.session.user.id // Assuming user ID is available in req.user
-
+  const citizenSession = req.session; // Assuming user ID is available in req.user
+  const citizenId = req.session.user.id; // Assuming user ID is available in req.user
 
   console.log("complaint received :", complaint);
-  console.log("complaint headers :",  complaintHeaders);
+  console.log("complaint headers :", complaintHeaders);
   console.log("citizen session :", citizenSession);
   console.log("citizen Id :", citizenId);
 
+  complaintList.push({
+    complaint: complaint.complaint,
+    status: "Pending",
+  });
 
   res.status(200).json({
     success: true,
-    message: "Complaint submitted successfully"
+    message: "Complaint submitted successfully",
+  });
+}
+
+export async function myComplaints(req, res) {
+  const aiResult = await classifyComplaint(complaintList[complaintList.length - 1].complaint);
+
+  console.log("AI Classification Result:", aiResult);
+  res.json({
+    success: true,
+    complaints: complaintList,
   });
 }
